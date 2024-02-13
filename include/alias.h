30fc3e46a88350fb20684ddbf3e8574f22a05c17
MuxDiven <divenmax15@gmail.com>
2024-02-13 17:49:39 +0000

    created alias data structure and first few functions

--- include/alias.h
+++ include/alias.h
@@ -1,10 +1,19 @@
+#include "utils.h"
+
 typedef struct alias {
-  char *key; // original command
-  char **comand;
+  char *key;      // original command
+  char **command; // tokens(potential flags attached to alias)
+  struct alias *next;
 } alias;
 
-typedef struct alias *aliases;
+typedef struct alias **Aliases;
 
-aliases allocate_aliases();
-int save_alias();
+void allocate_aliases(); // init
+alias *new_alias(char *key, Tokens command, int num_tok);
+int add_alias(Aliases aliases, char *key, Tokens args, int num_tok);
+int save_alias(Aliases aliases, char *key, char *filename);
+int is_alias(Aliases aliases, char *key);
 int load_alias();
+Tokens get_command(Aliases aliases, char *key);
+alias rem_alias(Aliases aliases, char *key);
+void del_alias(Aliases aliases, char *key);
--- src/alias.c
+++ src/alias.c
@@ -1,10 +1,49 @@
 #include "../include/alias.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-aliases a = {{"cd", NULL}, {"getpath", NULL}, {"setpath", NULL} {NULL, NULL}};
+// aliases a = {{"cd", NULL}, {"getpath", NULL}, {"setpath", NULL}, {NULL,
+// NULL}};
+//
+alias *new_alias(char *key, Tokens command, int num_tok) {
+  alias *a = (alias *)malloc(sizeof(alias));
+  a->key = malloc(sizeof(char) * (strlen(key) + 1));
+  strcpy(a->key, key);
+  a->command = copy_tokens(command, num_tok);
+  a->next = NULL;
+  return a;
+}
 
-aliases allocate_aliases() {
-  aliases a;
-  a->
+void allocate_aliases() {
+  Aliases aliases = malloc(sizeof(Aliases));
+  *aliases = NULL;
+}
+
+int add_alias(Aliases aliases, char *key, Tokens args, int num_tok) {
+  alias *temp, *head;
+  temp = new_alias(key, args, num_tok);
+
+  if (!(*aliases)) {
+    *aliases = temp;
+    return 0;
+  } else {
+    head = *aliases;
+    while (head->next) {
+      head = head->next;
+    }
+    head->next = temp;
+    return 0;
+  }
+  return 1;
+}
+
+Tokens get_command(Aliases aliases, char *key) {
+  alias *head = *aliases;
+  for (head; head != NULL; head = head->next) {
+    if (strcmp(head->key, key) == 0) {
+      return head->command;
+    }
+  }
+  return NULL;
 }
