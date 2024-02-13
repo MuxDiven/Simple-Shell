c82c62a52951c752a59a3a16075fc749b4bf4fd9
ethanmidgley <ethan.c.midgley@gmail.com>
2024-02-13 14:32:53 +0000

    Better readability

--- src/history.c
+++ src/history.c
@@ -3,7 +3,7 @@
 
 history *allocate_history() {
   history *h = (history *)malloc(sizeof(history));
-  h->previous_commands = (char ***)malloc(sizeof(char **) * HISTORY_SIZE);
+  h->previous_commands = (Tokens *)malloc(sizeof(Tokens) * HISTORY_SIZE);
   h->index = 0;
   h->size = HISTORY_SIZE;
 
