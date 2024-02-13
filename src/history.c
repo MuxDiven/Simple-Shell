c76f5283bd94af8a55a13cfffc906ebc606eeec6
ethanmidgley <ethan.c.midgley@gmail.com>
2024-02-13 14:31:16 +0000

    history type signatures

--- src/history.c
+++ src/history.c
@@ -3,5 +3,9 @@
 
 history *allocate_history() {
   history *h = (history *)malloc(sizeof(history));
+  h->previous_commands = (char ***)malloc(sizeof(char **) * HISTORY_SIZE);
+  h->index = 0;
+  h->size = HISTORY_SIZE;
+
   return h;
 }
