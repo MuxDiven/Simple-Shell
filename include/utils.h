70d1b599d10f4d544a9759dc97b763afc65029e8
calumvc <calum.cardownie.2022@uni.strath.ac.uk>
2024-02-27 23:08:57 +0000

    show history and utils

--- include/utils.h
+++ include/utils.h
@@ -3,3 +3,4 @@ typedef char **Tokens;
 char **input_tok(char *input, int *num_tokens);
 char **copy_tokens(Tokens tokens);
 void free_tokens(Tokens tokens);
+char *tokens_to_string(Tokens tokens);
--- src/history.c
+++ src/history.c
@@ -2,43 +2,54 @@
 #include "../include/utils.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 History allocate_history() {
-  History h = (History)malloc(sizeof(History));
+  History h = (History)malloc(sizeof(struct h));
   h->previous_commands = (Tokens *)malloc(sizeof(Tokens) * HISTORY_SIZE);
   h->index = 0;
+  h->counter = 0;
   h->size = HISTORY_SIZE;
 
   return h;
 }
 
-void add_history(History history, Tokens tokens,
-                 int num_tok) { // adds function used to history struct
-  if (history->index % history->size == 0) {
-    history->index -= history->size;
-  }
+void add_history(History history,
+                 Tokens tokens) { // adds function used to history struct
   if (history->previous_commands[history->index] == NULL) {
     history->previous_commands[history->index] = copy_tokens(tokens);
-    history->index++;
   } else {
-    // TODO:
     free_tokens(history->previous_commands[history->index]);
     history->previous_commands[history->index] = copy_tokens(tokens);
-    history->index++;
+  }
+  history->index++;
+  history->counter++;
+  if (history->index == history->size) {
+    history->index = 0;
   }
 }
 
 Tokens get_history(History history, int index);
 
 void show_history(History history) {
-  printf("History command invoked\n");
-  for (int i = 0; i < history->index; i++) {
-    printf("%d %s\n", i, history->previous_commands[i][0]);
+  int j = history->index;
+  if (history->previous_commands[history->index + 1] != NULL) {
+    for (int i = 0; i < 20; i++) {
+      printf("%d %s\n", i + 1, tokens_to_string(history->previous_commands[j]));
+      j++;
+      if (j == 20) {
+        j = 0;
+      }
+    }
+  } else {
+    for (int i = 0; i < history->index; i++) {
+      printf("%d %s\n", i + 1, tokens_to_string(history->previous_commands[i]));
+    }
   }
 }
 
 void save_history(
     History history); // save history from instance of struct into file
 
-History load_history(
-    History history); // load history from file into instance of history struct
+History load_history(History history); // load history from file into instance
+                                       // of history struct
